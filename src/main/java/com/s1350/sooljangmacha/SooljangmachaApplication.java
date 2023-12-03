50a717e7f615e95184e3d3a8a88b1059c2dc50d9
sojungpp <sophie2458@naver.com>
2023-12-03 20:44:44 +0900

    project setting

---
+++ src/main/java/com/s1350/sooljangmacha/SooljangmachaApplication.java
@@ -0,0 +1,13 @@
+package com.s1350.sooljangmacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooljangmachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooljangmachaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s1350/sooljangmacha/SooljangmachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1350.sooljangmacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SooljangmachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
