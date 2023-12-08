c0a8e3973e7dbdb0cc4844d5f81f596e8234d5f0
sojungpp <sophie2458@naver.com>
2023-12-08 18:24:27 +0900

    #2 feat: BaseEntity 생성

---
+++ src/main/java/com/s1350/sooljangmacha/global/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.s1350.sooljangmacha.global.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    @Setter
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT true")
+    private Boolean isEnable = true;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updateAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateAt = LocalDateTime.now();
+    }
+
+}
+
