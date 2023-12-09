a98bc9318f9813fadadc8600628c0351cb835161
sojungpp <sophie2458@naver.com>
2023-12-09 14:45:44 +0900

    #2 swagger 인증 추가 설정

--- src/main/java/com/s1350/sooljangmacha/global/config/SwaggerConfig.java
+++ src/main/java/com/s1350/sooljangmacha/global/config/SwaggerConfig.java
@@ -1,20 +1,42 @@
 package com.s1350.sooljangmacha.global.config;
 
 // Java
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .title("술병마차 API")
+                .description("술병마차 API 명세서")
+                .version("1.0.0");
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("JWT");
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("Bearer", bearerAuth))
+                .addSecurityItem(addSecurityItem)
+                .info(info);
     }
+
+
 
     private Info apiInfo() {
         return new Info()
