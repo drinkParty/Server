c5720d0a40806f6473c5bb85601f6dff6a4e08ca
chaerlo127 <monynony0203@gmail.com>
2023-12-09 16:28:19 +0900

    #4 fix: 포장마차 후기 생성 API valid message 추가

--- src/main/java/com/s1350/sooljangmacha/store/dto/request/PostStoreReviewReq.java
+++ src/main/java/com/s1350/sooljangmacha/store/dto/request/PostStoreReviewReq.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostStoreReviewReq {
-    @Schema(type = "String", description = "후기", example = "너무너무 맛있습니다!")
-    @NotBlank
+    @Schema(type = "String", description = "후기", example = "너무너무 맛있습니다!", required = true)
+    @NotBlank(message = "후기 내용을 입력해주세요.")
     private String content;
 }
