bd9210e288828dadc62e8712b888a772929f9cbf
sojungpp <sophie2458@naver.com>
2023-12-11 18:07:03 +0900

    #2 fix: webConfig 수정

--- src/main/java/com/s1350/sooljangmacha/global/config/WebConfig.java
+++ src/main/java/com/s1350/sooljangmacha/global/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.s1350.sooljangmacha.global.config;
 
 import com.s1350.sooljangmacha.global.resolver.UserAccountResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userAccountResolver);
+    }
+
+    @Bean
+    public WebSecurityCustomizer ignoringCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
     }
 }
